ef9b41ca52c3d6b4843b3786421d78b1ef1f830b
Errietta Kostala <errietta@errietta.me>
2015-03-20 11:49:50 +0000

    Let's move this to its own repo

--- example/gplustest.psgi
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-
-use gplustest;
-
-my $app = gplustest->apply_default_middlewares(gplustest->psgi_app);
-$app;
-
--- example/lib/gplustest/Model/DB.pm
+++
@@ -1,9 +0,0 @@
-package gplustest::Model::DB;
-
-use strict;
-use base 'Catalyst::Model::DBIC::Schema';
-
-__PACKAGE__->config(
-    schema_class => 'gplustest::DB',
-    AutoCommit => 1
-);
